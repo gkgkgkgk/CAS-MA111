372562f69397adae242db0c25dc40f5a83437fd3
gkgkgkgk <gkepets@gmail.com>
2019-11-06 16:11:38 -0500

    sync

--- matlab/integration.m
+++ matlab/integration.m
@@ -1,10 +1,7 @@
 syms f(x)
-f(x) = (2*x^4) - (17*x^3) + (46*x^2) - (43*x) + 12;
-df = diff(f, x);
-eq = df == 0;
+f(x) = ((2*x^4) - (17*x^3) + (46*x^2) - (43*x) + 12);
+eq = f(x) == 0;
 solution = solve(eq, x);
 disp(solution)
 
-incr = solve(df > 0, x);
-
-disp(incr)
+disp(solve(f(x) > 0));
